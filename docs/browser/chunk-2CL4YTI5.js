1669ef38edde8cfd25234d807fbf077c05ac6a0e
Nguyen Van Tien <nguyenvantien.coder@gmail.com>
2024-06-05 23:57:04 +0700

    docs

---
+++ docs/browser/chunk-2CL4YTI5.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-MFWVDWGZ.js";import"./chunk-W6GUCGWK.js";import"./chunk-MBUNWPJA.js";import"./chunk-ZHTFVYAQ.js";export{a as ProductdetailsComponent};
---
+++ docs/browser/chunk-XDH2UOK2.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-M3DUQ4Z6.js";import"./chunk-W6GUCGWK.js";import"./chunk-34P35LNV.js";import"./chunk-MBUNWPJA.js";import"./chunk-ZHTFVYAQ.js";export{a as ProductlistComponent};
